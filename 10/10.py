85d22fe903478786e684c5535cd23a3d5f69e4ad
Marc <marc.baetica@gmail.com>
2024-11-04 16:40:56 -0500

    Problem 10.

---
+++ 10/10.py
@@ -0,0 +1,38 @@
+# The sum of the primes below 10 is 2+3+5+7=17.
+# 
+# Find the sum of all the primes below two million.
+
+
+import math
+
+
+def is_prime(num):
+    for i in range(2, math.floor(math.sqrt(num))+1):
+        if num%i == 0:
+            return False
+    return True
+
+
+def generate_prime():
+    n = 1
+    while True:
+        n += 1
+        if is_prime(n):
+            yield n
+
+
+sum = 0
+# threshold = 10
+threshold = 2*10**6
+
+
+primes = generate_prime()
+while True:
+    prime = next(primes)
+    # print(f'New prime found: {prime}')
+    if prime > threshold:
+        break
+    sum += prime
+    # print(f'New sum is: {sum}')
+print(sum)
+
